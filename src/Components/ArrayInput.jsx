1f10d7f377d6faea74b54a91883059da4c4cb718
Rob Catton <robmcatton@gmail.com>
2020-06-06 23:09:40 +0100

    Gave seed a default value.

--- src/Components/ArrayInput.jsx
+++ src/Components/ArrayInput.jsx
@@ -5,7 +5,11 @@ const ArrayInput = ({numberOfInputs, type, register}) => {
     let arrays = [];
 
     for(let i = 0; i < numberOfInputs; i++){
-        arrays.push(<input class="" id={type+(i+1)} name={type+(i+1)} type="text" ref={register}/>)
+      let defValue = "";
+      if(type==="Seed"){
+        defValue=i+1;
+      }
+      arrays.push(<input class="" id={type+(i+1)} name={type+(i+1)} type="text" ref={register} defaultValue={defValue}/>)
     }   
 
   return (

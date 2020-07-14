3c37b5cf3b72ce055b459697b098514e47354057
anroebuck <aroebuck@quantbet.com>
2020-07-14 17:22:49 +0100

    add right click menu to search results

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { parseImgSource } from "./utils";
 import { useDispatch } from "react-redux";
-import { addCardToMain } from "../store/deckBuilder-actions";
+import { addCardToMain, incrementToX } from "../store/deckBuilder-actions";
+import useRefCreator from "./useRefCreator";
+import ContextMenu from "./ContextMenu";
 
 const SearchResult = ({ card, covered }) => {
+
+  const [ref, updateRef] = useRefCreator();
+
   const dispatch = useDispatch();
 
   const handleClick = () => dispatch(addCardToMain(card));
 
+  const contextMenuItems = [
+    { label: `Add 1`,
+      callback: () => dispatch(addCardToMain(card)) },
+    { label: `Increase to 4`,
+      callback: () => dispatch(incrementToX(card, 4)) },
+  ];
+
   return (
-    <div className={`search-result${covered && ' covered'}`} onClick={handleClick}>
-      <img className="search-result-img" src={parseImgSource(card)} alt={`search result for ${card.name}`}/>
+    <div className={`search-result${covered && ' covered'}`} ref={updateRef} >
+      <ContextMenu containerRef={ref} menuItems={contextMenuItems}/>
+      <img className="search-result-img" src={parseImgSource(card)} alt={`search result for ${card.name}`} onClick={handleClick} />
     </div>
   );
 };

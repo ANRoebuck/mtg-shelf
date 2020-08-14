63ce9563d2004e2c48df2e738abc1723eec8136f
anroebuck <aroebuck@quantbet.com>
2020-08-14 23:27:15 +0100

    lint

--- src/deckbuilder/components/Finance.jsx
+++ src/deckbuilder/components/Finance.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { viewBy } from "../enums";
-import ViewImages from './ViewImages';
-import ViewPrintable from './ViewPrintable';
-import ViewStats from './ViewStats';
 
 const Finance = ({ finance }) => {
 

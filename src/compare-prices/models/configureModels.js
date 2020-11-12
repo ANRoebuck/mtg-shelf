70079e9b56f29bbef4aaf3d4c33666a19e67bf46
anroebuck <aroebuck@quantbet.com>
2020-11-12 01:13:12 +0000

    added MCT and BOC

--- src/compare-prices/models/configureModels.js
+++ src/compare-prices/models/configureModels.js
@@ -1,13 +1,17 @@
 import ModelAxion from "./model-axion";
 import ModelChaosCards from "./model-chaos-cards";
-import ModelMadHouse from "./model-madhouse";
+import ModelMagicMadHouse from "./model-magic-mad-house";
 import ModelPatriotGamesLeeds from "./model-patriot-games-leeds";
 import ModelTrollTrader from "./model-trolltrader";
+import ModelMagicCardTrader from "./model-magic-card-trader";
+import ModelBigOrbitCards from "./model-big-orbit-cards";
 
 export const configureModels = () => [
   new ModelAxion(),
   // new ModelChaosCards(),
-  new ModelMadHouse(),
+  new ModelBigOrbitCards(),
+  new ModelMagicCardTrader(),
+  new ModelMagicMadHouse(),
   new ModelPatriotGamesLeeds(),
   new ModelTrollTrader(),
 ].map(model => {
---
+++ src/compare-prices/models/model-magic-card-trader.js
@@ -0,0 +1,115 @@
+import axios from 'axios';
+import { cors, regex, seller } from "./utils";
+
+class ModelMagicCardTrader {
+
+  parser = new DOMParser();
+
+  name = seller.magicCardTrader.name;
+  logo = seller.magicCardTrader.logo;
+  baseUrl = 'https://www.themagiccardtrader.com/';
+  searchPath = 'products/search?q=';
+
+  search = async (searchTerm) => {
+    const foundItems = [];
+    const resultNodes = await this.allResults(searchTerm);
+    resultNodes.forEach(resultNode => {
+      foundItems.push({
+        name: this.name,
+        logo: this.logo,
+        title: this.nameFromResultNode(resultNode),
+        price: this.priceFromResultNode(resultNode),
+        stock: this.stockFromResultNode(resultNode),
+        imgSrc: this.imgSrcFromResultNode(resultNode),
+        productRef: this.baseUrl + this.productRefFromResultNode(resultNode),
+        expansion: this.expansionFromResultNode(resultNode),
+      });
+    });
+    return foundItems;
+  }
+
+  getHtml = (searchTerm) => axios.get(this.searchTermToUrl(searchTerm)).catch(() => []);
+
+  searchTermToUrl = searchTerm => cors + this.baseUrl + this.searchPath
+    + searchTerm.toLowerCase().split(' ').join('+');
+
+  allResults = async (searchTerm) => {
+    return this.getHtml(searchTerm)
+      .then(({data: html}) => {
+        const document = this.parser.parseFromString(html, "text/html");
+        return document.querySelectorAll('div.products-container > ul > li')
+      });
+  }
+
+  nameFromResultNode = (resultNode) => {
+    let arr = [];
+    resultNode.querySelectorAll('div.inner > div.image-meta > div.meta > a > h4.name')
+      .forEach(node => {
+        let str = node.innerHTML.replace(regex.whitespaceStripper, `$2`);
+        arr.push(str);
+      });
+    return arr[0];
+  }
+
+  priceFromResultNode = (resultNode) => {
+    let arr = [];
+    resultNode.querySelectorAll('div.inner > div.variants > div.variant-row > span.variant-buttons > form > div.product-price-qty > span')
+      .forEach(node => {
+        const text = node.innerHTML;
+        arr.push({
+          text,
+          value: this.priceValueFromPriceText(text),
+        });
+      });
+    arr.push({text: '', value: 9999});
+    return arr[0];
+  }
+  priceValueFromPriceText = (text) => text ? parseInt(text.replace(/[£.]/g, ``)) : 9999;
+
+  stockFromResultNode = (resultNode) => {
+    let arr =[];
+    resultNode.querySelectorAll('div.inner > div.variants > div.variant-row > span.variant-main-info > span.variant-qty')
+      .forEach(node => {
+        const text = node.innerHTML.replace(regex.whiteSpaceStripper, `$2`);
+        arr.push({
+          text,
+          value: this.stockValueFromStockText(text),
+        });
+      });
+    arr.push({text: 'Out of Stock', value: 0});
+    return arr[0];
+  }
+  stockValueFromStockText = (text) => text === 'Out of stock' ? 0 : parseInt(text.replace(/([0-9]*)([^0-9]*)/, `$1`));
+
+
+  imgSrcFromResultNode = (resultNode) => {
+    let arr = [];
+    resultNode.querySelectorAll('div.inner > div.image-meta > div.image > a > img')
+      .forEach(node => {
+        arr.push(node.getAttribute('src'));
+      });
+    return arr[0];
+  }
+
+  productRefFromResultNode = (resultNode) => {
+    let arr = [];
+    resultNode.querySelectorAll('div.inner > div.image-meta > div.image > a')
+      .forEach(node => {
+        arr.push(node.getAttribute('href'));
+      });
+    return arr[0];
+  }
+
+  expansionFromResultNode = (resultNode) => {
+    let arr = [];
+    resultNode.querySelectorAll('div.inner > div.image-meta > div.meta > a > span.category')
+      .forEach(node => {
+        arr.push(node.innerHTML);
+      });
+    return arr[0];
+    return null;
+  }
+
+}
+
+export default ModelMagicCardTrader;

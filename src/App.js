e8381889710e9d32e91d2988c7092a027314f9f2
anroebuck <aroebuck@quantbet.com>
2020-11-19 23:42:13 +0000

    replace router with state and icons; add mana land box icons

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import './App.scss';
+import './app.scss';
 import DeckBuilder from "./deckbuilder/DeckBuilder";
 import Home from "./Home";
 import ComparePrices from "./compare-prices/ComparePrices";
+import NavigationIcon from "./navigation-bar/NavigationIcon";
 
 const App = () => {
 
+  const pages = {
+    home: 'Home',
+    deckBuilder: 'Deck Builder',
+    comparePrices: 'Compare Prices',
+    // draft: 'Draft',
+    // game: 'Game',
+  }
+
+  const [display, setDisplay] = useState(pages.home);
+
+  const navigationIcons = Object.values(pages).map((page, i) =>
+    <NavigationIcon page={page} value={i} active={display === page} setDisplay={setDisplay}/>);
+
+  const getPageToDisplay = () => {
+    switch (display) {
+      case pages.deckBuilder:
+        return <DeckBuilder/>;
+      case pages.comparePrices:
+        return <ComparePrices/>;
+      default:
+        return <Home/>;
+    }
+  }
+
+  const pageToDisplay = getPageToDisplay();
+
   return (
-    <Router>
-      <div className="App">
+    // <Router>
+    <div className="App">
+      {/*<ul className="menu">*/}
+      {/*  <li className="menu-item"><Link exact to={'/'}>Home</Link></li>*/}
+      {/*  <li className="menu-item"><Link to={'/deckBuilder'}>Deck Builder</Link></li>*/}
+      {/*  <li className="menu-item"><Link to={'/comparePrices'}>Compare Prices</Link></li>*/}
+      {/*</ul>*/}
 
-          <ul className="menu">
-            <li className="menu-item"><Link exact to={'/'}>Home</Link></li>
-            <li className="menu-item"><Link to={'/deckBuilder'}>Deck Builder</Link></li>
-            <li className="menu-item"><Link to={'/comparePrices'}>Compare Prices</Link></li>
-          </ul>
-
-          <hr/>
-
-          <Route exact path='/' component={Home}/>
-          <Route path='/deckBuilder' component={DeckBuilder}/>
-          <Route path='/comparePrices' component={ComparePrices}/>
-
+      <div className="menu">
+        {navigationIcons}
       </div>
-    </Router>
+
+      <hr/>
+
+      {pageToDisplay}
+
+      {/*<Route exact path='/' component={Home}/>*/}
+      {/*<Route path='/deckBuilder' component={DeckBuilder}/>*/}
+      {/*<Route path='/comparePrices' component={ComparePrices}/>*/}
+    </div>
+    // </Router>
   );
 }
 

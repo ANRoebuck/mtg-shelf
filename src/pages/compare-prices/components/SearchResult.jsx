aa7b7d6bbaa1babb1d380e2c43797d27110d6d26
anroebuck <aroebuck@quantbet.com>
2021-05-02 10:59:43 +0100

    more styling updates for compare prices

--- src/pages/compare-prices/components/SearchResult.jsx
+++ src/pages/compare-prices/components/SearchResult.jsx
@@ -9,25 +9,30 @@ const SearchResult = (discoveredPrice) => {
 
   return (
     <div className="discovered-price" data-in-stock={stock.value > 0}>
+
       <div className="seller">
         <img className="logo" src={logo} alt={name}/>
         {/*<div>{price.value}</div>*/}
         {/*<div>{stock.value}</div>*/}
       </div>
+
       <div className="details">
         <div className="name">{title}</div>
         <div className="expansion">{expansion}</div>
       </div>
-      <div className="stock">
-        <div>{stock.text}</div>
-      </div>
-      <div className="price">
-        <div>{price.text}</div>
-      </div>
+
       <div className="img-container">
         <img className="discovered-price-img" src={imgSrc} alt={name}/>
-        <ShoppingCartIcon onClick={onClickLink} />
       </div>
+
+      <div className="stock-info">
+        <div className="price">{price.text}</div>
+        <div className="stock-count">{stock.text}</div>
+        <div className="product-link">
+          <ShoppingCartIcon onClick={onClickLink}/>
+        </div>
+      </div>
+
     </div>
   )
 };

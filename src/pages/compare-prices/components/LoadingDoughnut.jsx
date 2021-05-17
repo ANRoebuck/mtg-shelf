9e8a4a7f34c5cc4d4c6837d1b9cc09c15af648d0
anroebuck <aroebuck@quantbet.com>
2021-05-17 01:18:49 +0100

    add loading doughnut

---
+++ src/pages/compare-prices/components/LoadingDoughnut.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Pie } from 'react-chartjs-2';
+
+const LoadingDoughnut = ({loaded, total}) => {
+
+  const data = {
+    datasets: [{
+      data: [loaded, total - loaded],
+      backgroundColor: ['green', 'black'],
+      borderWidth: [1, 1],
+      borderColor: 'black',
+    }],
+  };
+
+  const options = {
+    cutoutPercentage: 40,
+    responsive: false,
+    maintainAspectRatio: false,
+    tooltips: {
+      enabled: false
+    }
+  };
+
+  const height = 100;
+  const width = 100;
+
+  return (
+    <div className="loading-doughnut">
+      { loaded === total ? null :
+        <Pie data={data} options={options} height={height} width={width}/>
+      }
+    </div>
+  )
+};
+
+export default LoadingDoughnut;

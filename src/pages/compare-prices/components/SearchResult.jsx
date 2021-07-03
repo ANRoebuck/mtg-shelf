ba09cda6c4a690215e54a4fac7c07a60fae1d2fc
anroebuck <aroebuck@quantbet.com>
2021-07-03 23:45:02 +0100

    finally learning how to css

--- src/pages/compare-prices/components/SearchResult.jsx
+++ src/pages/compare-prices/components/SearchResult.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import FoilStar from "./FoilStar";
 
 const SearchResult = (discoveredPrice) => {
 
-  const {name, logo, title, price, stock, imgSrc, productRef, expansion} = discoveredPrice;
+  const {name, logo, title, price, stock, imgSrc, productRef, expansion, isFoil} = discoveredPrice;
 
   const onClickLink = () => window.open(productRef, "_blank");
 
@@ -11,17 +12,16 @@ const SearchResult = (discoveredPrice) => {
     <div className="discovered-price" data-in-stock={stock.value > 0}>
 
       <img className="logo" src={logo} alt={name}/>
+      {isFoil ? <FoilStar/> : null}
       <div className="name">{title}</div>
       <div className="expansion">{expansion}</div>
 
-      <div className="img-container">
+      <div className="discovered-price-img-container">
         <img className="discovered-price-img" src={imgSrc} alt={name}/>
       </div>
 
-      <div className="stock-details">
-        <div className="price">{price.text}</div>
-        <div className="stock-count">{stock.text}</div>
-      </div>
+      <div className="price">{price.text}</div>
+      <div className="stock-count">{stock.text}</div>
 
       <div className="product-link">
         <ShoppingCartIcon onClick={onClickLink}/>

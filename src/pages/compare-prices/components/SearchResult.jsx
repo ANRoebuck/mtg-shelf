13e7cd54f25f8a55cb00a36af35fcf2698936cd6
anroebuck <aroebuck@quantbet.com>
2021-07-04 03:21:56 +0100

    fancypants saved cards menu

--- src/pages/compare-prices/components/SearchResult.jsx
+++ src/pages/compare-prices/components/SearchResult.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import FoilStar from "./FoilStar";
 
-const SearchResult = (discoveredPrice) => {
+const SearchResult = (discoveredPrice, isSaved, addSavedPrice, removeSavedPrice) => {
 
   const {name, logo, title, price, stock, imgSrc, productRef, expansion, isFoil} = discoveredPrice;
+
+  const onClickSaveWidget = () => {
+    if (!isSaved) addSavedPrice(discoveredPrice);
+    else removeSavedPrice(discoveredPrice);
+  }
 
   const onClickLink = () => window.open(productRef, "_blank");
 
@@ -12,19 +19,31 @@ const SearchResult = (discoveredPrice) => {
     <div className="discovered-price" data-in-stock={stock.value > 0}>
 
       <img className="logo" src={logo} alt={name}/>
+
       {isFoil ? <FoilStar/> : null}
+
       <div className="name">{title}</div>
+
       <div className="expansion">{expansion}</div>
 
       <div className="discovered-price-img-container">
         <img className="discovered-price-img" src={imgSrc} alt={name}/>
       </div>
 
-      <div className="price">{price.text}</div>
       <div className="stock-count">{stock.text}</div>
 
-      <div className="product-link">
-        <ShoppingCartIcon onClick={onClickLink}/>
+      <div className="price">{price.text}</div>
+
+      <div className="widgets">
+        <div className="bookmark">
+          {isSaved
+            ? <DeleteForeverIcon onClick={onClickSaveWidget} />
+            : <SaveIcon onClick={onClickSaveWidget} />}
+        </div>
+
+        <div className="product-link">
+          <ShoppingCartIcon onClick={onClickLink}/>
+        </div>
       </div>
 
     </div>

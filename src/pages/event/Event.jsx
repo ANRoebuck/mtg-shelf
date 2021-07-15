ec792f7478208a12eb3e1af85f0f8e9a21a3a33f
anroebuck <aroebuck@quantbet.com>
2021-07-15 02:29:25 +0100

    implementing event manager

--- src/pages/event/Event.jsx
+++ src/pages/event/Event.jsx
@@ -1,46 +1,83 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Pairing from "./Pairing";
+import './event.scss';
 
 
-const baseUrl = 'http://localhost:9090/api/eventManager/';
+const host = 'https://mtg-shelf-event-manager.herokuapp.com/'
+const localHost = 'http://localhost:9090/';
+const baseUrl = host + 'api/eventManager/';
 
 const Event = () => {
 
   const [eventId, setEventId] = useState(null);
   const [pairings, setPairings] = useState([]);
+  const [completedMatches, setCompletedMatches] = useState([]);
 
 
-  const createEvent = () => axios.get(baseUrl + 'new/12').then(({data}) => {
+  const createEvent = () => axios.get(baseUrl + 'new/50').then(({data}) => {
     console.log(data);
     setEventId(data.eventId);
   });
 
-  const getPairings = () => axios.get(baseUrl + `pairings/${eventId}/1`).then(({data}) => {
-    console.log(data);
-    setPairings(data.pairings)
-  });
+  const getPairings = () => axios
+    .get(baseUrl + `currentRound/${eventId}`)
+    .then(({data}) => axios.get(baseUrl + `pairings/${eventId}/${data.currentRound}`))
+    .then(({data}) => {
+      console.log(data);
+      setCompletedMatches([]);
+      setPairings(data.pairings);
+      markByeComplete(data.pairings);
+    });
+
+  const markByeComplete = (pairings) => {
+    const bye = pairings.filter(([p1, p2]) => [p1.player, p2.player].includes("BYE"))[0] || null;
+    if (bye) completePairing(bye);
+  }
+
+  const submitResultForPairing = (pairing, playerId, result) =>
+    axios.post(baseUrl + `result/${eventId}/${playerId}/${result}`)
+      .then((r) => completePairing(pairing));
+
+  const completePairing = (pairing) => {
+    setPairings(prev => prev.filter(p => p != pairing));
+    setCompletedMatches(prevState => {
+      const updatedState = [...prevState];
+      updatedState.push(pairing);
+      return updatedState;
+    });
+  }
 
   return (
     <div className="event">
 
       {eventId ?
         <div className="event-button">
-          EventId: {eventId}
+
         </div>
         :
-        <div className="event-button" onClick={() => createEvent()}>
-          Create new event
+        <div className="event-button" >
+          <button onClick={() => createEvent()}>
+            Create new event
+          </button>
         </div>
       }
 
 
-      <div className="pairings-button" onClick={() => getPairings()}>
-        Get pairings
-      </div>
+      {eventId && pairings.length === 0 ?
+        <div className="pairings-button">
+          <button onClick={() => getPairings()}>
+            Get pairings
+          </button>
+        </div>
+        : null}
 
       <div className="pairings">
-        {pairings.map(pairing => <Pairing pairing={pairing}/>)}
+        {pairings.map(pairing =>
+          <Pairing pairing={pairing} submitResultForPairing={submitResultForPairing} completed={false}/>)}
+
+        {completedMatches.map(pairing =>
+          <Pairing pairing={pairing} submitResultForPairing={submitResultForPairing} completed={true}/>)}
       </div>
 
     </div>
--- src/pages/event/Pairing.jsx
+++ src/pages/event/Pairing.jsx
@@ -3,33 +3,38 @@ import axios from 'axios';
 import './pairing.scss';
 
 
-const Pairing = ({pairing}) => {
+const Pairing = ({pairing, submitResultForPairing, completed}) => {
   const [p1, p2] = pairing;
+
+  const p1Name = p1.player === "BYE" ? "BYE" : `${p1.player} : (${p1.points}pts)`;
+  const p2Name = p2.player === "BYE" ? "BYE" : `${p2.player} : (${p2.points}pts)`;
+  const matchIsBye = [p1Name, p2Name].includes("BYE");
+
   return (
-    <div className="pairing">
+    <div className={`pairing ${completed ? 'completed' : ''}`}>
 
       <div className="button-container">
-        <button>
+        <button onClick={() => submitResultForPairing(pairing, p1.player, p1.player)} disabled={matchIsBye}>
           Win
         </button>
       </div>
 
       <div className="player">
-        {`Player ${p1.player} (${p1.points}pts)`}
+        {p1Name}
       </div>
 
       <div className="button-container">
-        <button>
+        <button onClick={() => submitResultForPairing(pairing, p1.player, "DRAW")} disabled={matchIsBye}>
           Draw
         </button>
       </div>
 
       <div className="player">
-        {`Player ${p2.player} (${p1.points}pts)`}
+        {p2Name}
       </div>
 
       <div className="button-container">
-        <button>
+        <button onClick={() => submitResultForPairing(pairing, p1.player, p2.player)} disabled={matchIsBye}>
           Win
         </button>
       </div>

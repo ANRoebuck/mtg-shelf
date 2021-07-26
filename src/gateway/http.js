4557a1681f34f1501b0701347b00e76d6728050e
anroebuck <aroebuck@quantbet.com>
2021-07-26 20:51:26 +0100

    implement autosuggesting search bar

--- src/gateway/http.js
+++ src/gateway/http.js
@@ -7,6 +7,10 @@ export const searchCards = (searchTerm) => axios
   .get(`${scryfall}/cards/search?q=${searchTerm}`)
   .then(({ data }) => data.data);
 
+export const autocomplete = (searchTerm) => axios
+  .get(`${scryfall}/cards/autocomplete?q=${searchTerm}`)
+  .then(({ data }) => data.data);
+
 export const getImgBytes = (imgUri) => axios
   .get(imgUri)
   .then(({ data }) => data);

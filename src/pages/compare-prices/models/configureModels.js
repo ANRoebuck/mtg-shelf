7c87c73c946791fda97a6d8940bb1bcec40e4069
anroebuck <aroebuck@quantbet.com>
2021-08-07 11:33:31 +0100

    re-enable models

--- src/pages/compare-prices/models/configureModels.js
+++ src/pages/compare-prices/models/configureModels.js
@@ -8,14 +8,14 @@ import ModelPatriotGamesLeeds from './ModelPatriotGamesLeeds';
 import ModelTrollTrader from './ModelTrollTrader';
 
 export const configureModels = () => [
-  // new ModelAxion(),
-  // new ModelBigOrbitCards(),
-  // new ModelMagicCardTrader(),
-  // new ModelMagicMadhouse(),
-  // new ModelManaLeak(),
+  new ModelAxion(),
+  new ModelBigOrbitCards(),
+  new ModelMagicCardTrader(),
+  new ModelMagicMadhouse(),
+  new ModelManaLeak(),
   new ModelMountBatten(),
-  // new ModelPatriotGamesLeeds(),
-  // new ModelTrollTrader(),
+  new ModelPatriotGamesLeeds(),
+  new ModelTrollTrader(),
 ].map(model => {
     return {
       name: model.name,

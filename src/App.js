8c6ad44a740d863efb001b373736fa52b8ce5c10
anroebuck <aroebuck@quantbet.com>
2021-11-22 21:40:36 +0000

    revert main app page for merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import './app.scss';
 import NavBar from './navigation-bar/NavBar';
-import DeckBuilder from "./pages/deckbuilder/DeckBuilder";
-import Home from "./pages/home/Home";
-import ComparePrices from "./pages/compare-prices/ComparePrices";
-import Draft from "./pages/draft/Draft";
-import Game from "./pages/game/Game";
+import DeckBuilder from './pages/deckbuilder/DeckBuilder';
+import Home from './pages/home/Home';
+import ComparePrices from './pages/compare-prices/ComparePrices';
+import Draft from './pages/draft/Draft';
+import Game from './pages/game/Game';
 
 const App = () => {
 
@@ -39,8 +39,10 @@ const App = () => {
   return (
     <div className="App">
 
+      <NavBar pages={pages} selected={selected} setSelected={setSelected}/>
+
       <div className="page">
-        <ComparePrices/>
+        {pageToDisplay}
       </div>
 
     </div>
